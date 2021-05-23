3db8b285aaf3ac8d5428b0d298f3a3afca2c993d
GoTo15 <goto15pod@gmail.com>
2021-05-23 12:17:30 -0500

    Add book and member migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 5) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.integer "member_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "clubs", primary_key: "uuid", force: :cascade do |t|
     t.string "name"
@@ -20,6 +28,13 @@ ActiveRecord::Schema.define(version: 3) do
 
   create_table "lcubmembers", force: :cascade do |t|
     t.integer "member_id"
+    t.integer "club_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "meetings", force: :cascade do |t|
+    t.string "name"
     t.integer "club_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
