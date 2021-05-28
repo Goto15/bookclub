0f5a5de7c04afe34d7e974283b9c6f3a84c73c28
GoTo15 <goto15pod@gmail.com>
2021-05-27 21:36:11 -0500

    Add MainContent component, create initial main app page structure

---
+++ frontend/src/components/mainContent.tsx
@@ -0,0 +1,5 @@
+export default function MainContent(){
+  return (
+    <div className="w-9/12">Hello world</div>
+  )
+}
--- frontend/src/pages/appMain.tsx
+++ frontend/src/pages/appMain.tsx
@@ -1,11 +1,12 @@
 /* ----- COMPONENTS ----- */
 import Sidebar from '../components/sidebar'
+import MainContent from '../components/mainContent'
 
 export default function AppMain(){
   return (
     <div className="flex flex-wrap w-full h-screen sm:flex-row sm:justify-around">
       <Sidebar></Sidebar>
-      <div className="w-9/12">Hello World!</div>
+      <MainContent></MainContent>
     </div>
   )
 }
